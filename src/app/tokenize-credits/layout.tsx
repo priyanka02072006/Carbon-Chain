e33fdde22dc600da80910498fe96ca2804a806f5
Priyanka <mpri.csbs2024@rmd.ac.in>
2026-06-27 12:56:29 +0530

    Create layout.tsx

---
+++ src/app/tokenize-credits/layout.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import type { ReactNode } from 'react';
+import { MainLayout } from '@/components/layout/main-layout';
+
+export default function TokenizeCreditsLayout({ children }: { children: ReactNode }) {
+  return (
+    <MainLayout>
+      {children}
+    </MainLayout>
+  );
+}
